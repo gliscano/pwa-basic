dad53a1a095ca5b604e3a81c182b38739ded092d
Guillermo Rojas <ing.guillermorojas@gmail.com>
2020-02-06 14:59:04 -0300

    primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,19 @@
+
+
+
+
+function updateCacheDynamic( dynamicCache, req , resp ){
+
+    if( resp.ok ){
+        caches.open( dynamicCache ).then( cache => { 
+
+            cache.put( req, resp.clone() );
+            
+            return resp.clone();
+
+        });
+    }else{
+        // restornar la respuesta, no hay respuesta por error de conexion o recurso...
+        return resp;
+    }
+};
